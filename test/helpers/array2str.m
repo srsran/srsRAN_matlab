514871f93b7c82c139a8634711b551ff737a6596
dvdgrgrtt <davidg@srs.io>
2022-03-21 16:16:49 +0100

    Fix array2str
    Remove unused argument and fix if statement.

--- test/helpers/array2str.m
+++ test/helpers/array2str.m
@@ -8,12 +8,12 @@
 %    and returns
 %        * string OUTPUTSTRING - string generated from the input numeric values
 
-function outputString = array2str(inputArray, inputIsFloat)
-    if any(mod(inputArray,1))>0
+function outputString = array2str(inputArray)
+    if any(mod(inputArray,1) > 0)
         fmt = '%.3f';
     else
         fmt = '%d';
     end
     inputArray = inputArray(:)'; % ensure it's a row
-    outputString = [num2str(inputArray(1:end-1), [fmt, ', ']), ' ' num2str(inputArray(end), fmt)];
+    outputString = [num2str(inputArray(1:end-1), [fmt, ', ']), ' ', num2str(inputArray(end), fmt)];
   end
