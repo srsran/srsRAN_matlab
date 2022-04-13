94d8e3ec4c832b6ca7019d5c9262c910d07ae42b
dvdgrgrtt <davidg@srs.io>
2022-04-13 10:35:53 +0200

    Provide soft demodulator function

---
+++ +srsMatlabWrappers/+phy/+upper/+channel_modulation/srsDemodulator.m
@@ -0,0 +1,64 @@
+%srsDemodulator Soft demodulator for NR constellations.
+%   LLRS = srsDemodulator(SYMBOLS, SCHEME) computes the log-likelihood ratios LLRS
+%   corresponding to the (noisy) modulated symbols SYMBOLS, and the modulation
+%   scheme SCHEME. SYMBOLS is an array of complex values. SCHEME is a character
+%   array denoting one of the possible NR constellations, namely 'BPSK', 'QPSK',
+%   '16QAM', '64QAM', or '256QAM'.
+%
+%   LLRS = srsDemodulator(..., NOISEVAR) specifies the noise variance for the input
+%   symbols. If NOISEVAR is an array of positive values, then it must have the same
+%   number of elements as SYMBOLS (SYMBOLS have different noise variances). If
+%   NOISEVAR is a positive scalar, then it is assumed that the noise variance is
+%   the same across all SYMBOLS. The default is NOISEVAR = 1e-10.
+function llrs = srsDemodulator(symbols, scheme, noiseVar)
+    arguments
+        symbols (:, 1) double
+        scheme  (1, :) char {mustBeMember(scheme, {'BPSK', 'QPSK', '16QAM', '64QAM', '256QAM'})}
+        noiseVar (:, 1) double {mustBeReal, mustBePositive, mustBeFinite} = 1e-10
+    end
+
+    if (numel(noiseVar) > 1)
+        assert(size(symbols) == size(noiseVar), 'Input NOISEVAR has invalid size.');
+    end
+
+    % generate NR constellation
+    switch scheme
+        case 'BPSK'
+            nBits = 1;
+        case 'QPSK'
+            nBits = 2;
+        case '16QAM'
+            nBits = 4;
+        case '64QAM'
+            nBits = 6;
+        case '256QAM'
+            nBits = 8;
+    end
+
+    if strcmp(scheme, 'BPSK')
+        % Because of MATLAB limitations, we need to treat BPSK as QPSK and then combine
+        % the LLRs.
+        constLabels = [2 3 0 1];
+        nConstPoints = 4;
+
+        llrsTmp = qamdemod(symbols, nConstPoints, constLabels, ...
+            'UnitAveragePower', true, 'NoiseVariance', noiseVar, 'OutputType', 'approxllr');
+        llrs = llrsTmp(1:2:end) + llrsTmp(2:2:end);
+    else
+        nConstPoints = 2^nBits;
+
+        % we need to label the constellation points from the top-left corner, column order
+        % first compute the modulated symbols for all possible bit strings, according to the NR constellation
+        constPointsBits = int2bit(0:nConstPoints-1, nBits);
+        constPoints = nrSymbolModulate(constPointsBits(:), scheme);
+        % now, demodulate them to see the position they get in the constellation grid
+        tmp1 = qamdemod(constPoints, nConstPoints, 'bin', 'UnitAveragePower', true);
+        % finally, invert the map label-point to get the labels in the correct order
+        tmp2 = sortrows([tmp1, (0:nConstPoints-1)']);
+        constLabels = tmp2(:, 2);
+
+        % demodulate the symbols with the computed constellation
+        llrs = qamdemod(symbols, nConstPoints, constLabels, ...
+            'UnitAveragePower', true, 'NoiseVariance', noiseVar, 'OutputType', 'approxllr');
+    end
+
--- +srsMatlabWrappers/+phy/+upper/+channel_modulation/srsModulator.m
+++ +srsMatlabWrappers/+phy/+upper/+channel_modulation/srsModulator.m
@@ -1,10 +1,10 @@
 %srsModulator Generation of modulated symbols from an input bit array.
-%   [MODULATEDSYMBOLS] = srsModulator(CW, SHEME)
+%   MODULATEDSYMBOLS = srsModulator(CW, SCHEME)
 %   modulates the input bit sequence accordig to the requested SCHEME
 %   and returns the complex symbols MODULATEDSYMBOLS.
 %
 %   See also nrPBCHDMRS, nrPBCHDMRSIndices.
 
-function [modulatedSymbols] = srsModulator(cw, scheme)
+function modulatedSymbols = srsModulator(cw, scheme)
     modulatedSymbols = nrSymbolModulate(cw, scheme, 'OutputDataType', 'single');
 end
