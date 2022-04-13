2a053add2957efdaa3771d6713a2bcc696e7cec8
dvdgrgrtt <davidg@srs.io>
2022-04-13 10:40:14 +0200

    Provide writers for float and int8 binary files

---
+++ +srsTest/+helpers/writeFloatFile.m
@@ -0,0 +1,11 @@
+%writeFloatFile Writes real-valued float symbols to a binary file.
+%   writeFloatFile(FILENAME, DATA) generates a new binary file FILENAME
+%    containing a set of real-valued symbols, formatted to match the 'file_vector<float>'
+%    object used by the SRS gNB.
+
+function writeFloatFile(filename, data)
+    fileID = fopen(filename, 'w');
+    dataLength = length(data);
+    fwrite(fileID, data, 'float32');
+    fclose(fileID);
+end
---
+++ +srsTest/+helpers/writeInt8File.m
@@ -0,0 +1,11 @@
+%writeInt8File Generates a new binary file with 'int8_t' entries.
+%   writeInt8File(FILENAME, DATA) writes the numeric array DATA to the binary
+%   file FILENAME (pathname). The format matches the 'file_vector<int8_t>' object
+%   used by SRSGNB.
+
+function writeInt8File(filename, data)
+    fileID = fopen(filename, 'w');
+    dataLength = length(data);
+    fwrite(fileID, data, 'int8');
+    fclose(fileID);
+end
