a4c90b2bddc75e9f4c00ffb1c207d67d63661230
Xavier Arteaga <xavier@softwareradiosystems.com>
2022-06-30 17:35:28 +0200

    Reviewed write resource grid entries to file

--- +srsTest/+helpers/writeResourceGridEntryFile.m
+++ +srsTest/+helpers/writeResourceGridEntryFile.m
@@ -5,22 +5,22 @@
 %   used by the SRSGNB.
 
 function writeResourceGridEntryFile(filename, data, indices)
-    % Make sure data has a good format.
-    data = data(:);
+% Make sure data has a good format.
+data = data(:);
 
-    % Flatten coordinates in a 32bit register.
-    gridCoordinate = cast(indices(:, 3), 'uint32') ...
-        + cast(indices(:, 2), 'uint32') * 2^8 + ...
-        + cast(indices(:, 1), 'uint32') * 2^16;
+% Flatten coordinates in a 32bit register.
+gridCoordinate = uint32(indices(:, 3)) ...
+    + uint32(indices(:, 2)) * 2^8 + ...
+    + uint32(indices(:, 1)) * 2^16;
 
-    % Flatten data in a binary format·
-    singleRealData = zeros(1, 3 * numel(data), 'uint32');
-    singleRealData(1:3:end) = gridCoordinate;
-    singleRealData(2:3:end) = typecast(single(real(data)), 'uint32');
-    singleRealData(3:3:end) = typecast(single(imag(data)), 'uint32');
+% Flatten data in a binary format·
+singleRealData = zeros(1, 3 * numel(data), 'uint32');
+singleRealData(1:3:end) = gridCoordinate;
+singleRealData(2:3:end) = typecast(single(real(data)), 'uint32');
+singleRealData(3:3:end) = typecast(single(imag(data)), 'uint32');
 
-    % Write all data once.
-    fileID = fopen(filename, 'w');
-    fwrite(fileID, singleRealData, 'uint32');
-    fclose(fileID);
+% Write all data once.
+fileID = fopen(filename, 'w');
+fwrite(fileID, singleRealData, 'uint32');
+fclose(fileID);
 end
