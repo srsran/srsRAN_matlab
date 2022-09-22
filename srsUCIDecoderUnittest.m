b382dfc9da17ee2e34b3f554b00b34b2855ed6a2
ofontbach <oriol.font@srs.io>
2022-09-22 12:21:16 +0200

    Fixes MR comments

--- srsUCIDecoderUnittest.m
+++ srsUCIDecoderUnittest.m
@@ -18,11 +18,11 @@
 %   srsUCIDecoderUnittest Properties (TestParameter):
 %
 %   A                       - Length in bits of the UCI message.
-%   modScheme               - Modulation scheme (see extended documentation for details).
+%   modScheme               - Modulation scheme.
 %
 %   srsUCIDecoderUnittest Methods (TestTags = {'testvector'}):
 %
-%   testvectorGenerationCases - Generates a test vectors according to the provided
+%   testvectorGenerationCases - Generates a test vector according to the provided
 %                               parameters.
 %
 %   srsUCIDecoderUnittest Methods (Access = protected):
@@ -50,7 +50,7 @@ classdef srsUCIDecoderUnittest < srsTest.srsBlockUnittest
         %Length in bits of the UCI message. Four different cases are covered: single
         %   UCI bit (1), two UCI bits (2), length between 3 and 11 UCI bits (3-11)
         %   and length between 12 and 1706 UCI bits (12-1706).
-        A = {1, 2, randi([3 11], 1, 1), randi([12 1706], 1, 1)}
+        A = [num2cell(1:12) {19, 20, 200, 500, 1000, 1706}]
 
         %Modulation scheme, described as a three-entry cell array. The first
         %entry is the modulation order, the second and the third are the
@@ -64,14 +64,17 @@ classdef srsUCIDecoderUnittest < srsTest.srsBlockUnittest
         function addTestIncludesToHeaderFile(~, fileID)
         %addTestIncludesToHeaderFile Adds include directives to the test header file.
 
-            %fprintf(fileID, '#include "srsgnb/support/file_vector.h"\n');
-
+            fprintf(fileID, '#include "srsgnb/phy/upper/log_likelihood_ratio.h"\n');
+            fprintf(fileID, '#include "srsgnb/ran/modulation_scheme.h"\n');
+            fprintf(fileID, '#include "srsgnb/support/file_vector.h"\n');
         end
 
         function addTestDefinitionToHeaderFile(~, fileID)
         %addTestDetailsToHeaderFile Adds details (e.g., type/variable declarations) to the test header file.
 
             fprintf(fileID, 'struct test_case_t {\n');
+            fprintf(fileID, '  unsigned                          message_length = 0;\n');
+            fprintf(fileID, '  unsigned                          llr_length     = 0;\n');
             fprintf(fileID, '  uci_decoder::configuration        config;\n');
             fprintf(fileID, '  file_vector<uint8_t>              message;\n');
             fprintf(fileID, '  file_vector<log_likelihood_ratio> llr;\n');
@@ -87,65 +90,63 @@ classdef srsUCIDecoderUnittest < srsTest.srsBlockUnittest
             import srsTest.helpers.writeUint8File
             import srsTest.helpers.writeInt8File
 
-            % the current srsGNB implementation only supports UCI messages
-            % up to 11 bits
+            % The current srsGNB implementation only supports UCI messages
+            % up to 11 bits.
             isASupported = A < 12;
-
-            if (isASupported)
-                % Generate a unique test ID
-                testID = testCase.generateTestID;
-    
-                % set randomized values
-                UCIbits = randi([0 1], A, 1);
-                % the length of the rate-matched UCI codeword, E, depends on A and on
-                %     the modulation scheme (maximum length = 8192)
-                minE = A + 1;
-                % for sequences larger than 12 bits there will be 11 CRC bits
-                if A >= 12
-                    minE = A + 11;
-                end;
-                maxE = floor(8192 / modScheme{1});
-                if maxE < minE
-                    maxE = minE;
-                end;
-                E = modScheme{1} * randi([minE maxE], 1, 1);
-    
-                % current fixed parameter values (e.g., SNR)
-                snrdB = 20;
-    
-                % encode the UCI bits
-                UCICodeWord = nrUCIEncode(UCIbits, E, modScheme{2});
-    
-                % replace placeholders -1 (x) and -2 (y) as part of the descrambling.
-                UCICodeWord(UCICodeWord == -1) = 1;
-                UCICodeWord(UCICodeWord == -2) = UCICodeWord(find(UCICodeWord == -2) - 1);
-    
-                % estimate the LLR soft bits
-                modulatedUCI = nrSymbolModulate(UCICodeWord, modScheme{2});
-                rxSignal = awgn(modulatedUCI, snrdB);
-                LLRSoftBits = nrSymbolDemodulate(rxSignal, modScheme{2});
-    
-                % decode the received UCI LLR soft bits
-                decodedUCIBits = nrUCIDecode(LLRSoftBits, A, modScheme{2});
-    
-                % clip and quantize the LLRs
-                LLRSoftBits(LLRSoftBits > 20) = 20;
-                LLRSoftBits(LLRSoftBits < -20) = -20;
-                LLRSoftBits = round(LLRSoftBits * 6); % this is codeblocks * 120 / 20
-                % write the LLRs to a binary file
-                testCase.saveDataFile('_test_input', testID, @writeInt8File, LLRSoftBits(:));
-    
-                % write the decoded UCI message to a binary file
-                testCase.saveDataFile('_test_output', testID, @writeUint8File, decodedUCIBits);
-    
-                % generate the test case entry
-                testCaseString = testCase.testCaseToString(testID, ...
-                    {['modulation_scheme::', modScheme{3}], A, E}, ...
-                    true, '_test_output', '_test_input');
-    
-                % add the test to the file header
-                testCase.addTestToHeaderFile(testCase.headerFileID, testCaseString);
+            if (~isASupported)
+                return;
             end
+
+            % Generate a unique test ID.
+            testID = testCase.generateTestID;
+
+            % Set randomized values.
+            UCIbits = randi([0 1], A, 1);
+            % The length of the rate-matched UCI codeword, E, depends on A and on
+            % the modulation scheme (maximum length = 8192).
+            minE = A + 1;
+            % For sequences larger than 12 bits there will be 11 CRC bits.
+            if A >= 12
+                minE = A + 11;
+            end
+            maxE = max([floor(8192 / modScheme{1}), minE]);
+            E = modScheme{1} * randi([minE maxE], 1, 1);
+
+            % Current fixed parameter values (e.g., SNR).
+            snrdB = 20;
+
+            % Encode the UCI bits.
+            UCICodeWord = nrUCIEncode(UCIbits, E, modScheme{2});
+
+            % Replace placeholders -1 (x) and -2 (y) as part of the descrambling.
+            UCICodeWord(UCICodeWord == -1) = 1;
+            UCICodeWord(UCICodeWord == -2) = UCICodeWord(find(UCICodeWord == -2) - 1);
+
+            % Estimate the LLR soft bits.
+            modulatedUCI = nrSymbolModulate(UCICodeWord, modScheme{2});
+            rxSignal = awgn(modulatedUCI, snrdB);
+            LLRSoftBits = nrSymbolDemodulate(rxSignal, modScheme{2});
+
+            % Decode the received UCI LLR soft bits.
+            decodedUCIBits = nrUCIDecode(LLRSoftBits, A, modScheme{2});
+
+            % Clip and quantize the LLRs.
+            LLRSoftBits(LLRSoftBits > 20) = 20;
+            LLRSoftBits(LLRSoftBits < -20) = -20;
+            LLRSoftBits = round(LLRSoftBits * 6); % this is LLRSoftBits * 120 / 20
+            % Write the LLRs to a binary file.
+            testCase.saveDataFile('_test_input', testID, @writeInt8File, LLRSoftBits(:));
+
+            % Write the decoded UCI message to a binary file.
+            testCase.saveDataFile('_test_output', testID, @writeUint8File, decodedUCIBits);
+
+            % Generate the test case entry.
+            testCaseString = testCase.testCaseToString(testID, ...
+                {A, E, {['modulation_scheme::', modScheme{3}]}}, ...
+                false, '_test_output', '_test_input');
+
+            % Add the test to the file header.
+            testCase.addTestToHeaderFile(testCase.headerFileID, testCaseString);
         end % of function testvectorGenerationCases
     end % of methods (Test, TestTags = {'testvector'})
 end % of classdef srsUCIDecoderUnittest
