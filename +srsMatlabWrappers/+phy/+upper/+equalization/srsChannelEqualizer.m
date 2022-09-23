87e84dd5acd993feb47b87a44c1e11fdf0909545
Joaquim Broquetas <joaquim.broquetas@srs.io>
2022-09-23 19:02:49 +0200

    fix srsChannelEqualizer documentation

--- +srsMatlabWrappers/+phy/+upper/+equalization/srsChannelEqualizer.m
+++ +srsMatlabWrappers/+phy/+upper/+equalization/srsChannelEqualizer.m
@@ -1,13 +1,14 @@
 %srsChannelEqualizer 5G NR MIMO Channel Equalizer.
 %   
-%   [EQSYMBOLS, EQNOISEVARS] = srsChannelEqualizer(RXSYMBOLS, CHESTS, EQTYPE, NOISEVAR)
+%   [EQSYMBOLS, EQNOISEVARS] = srsChannelEqualizer(RXSYMBOLS, CHESTS, EQTYPE, NOISEVAR, TXSCALING)
 %   Equalizes the Resource Elements (RE) in RXSYMBOLS using the estimated
 %   channel information CHESTS and the noise variance NOISEVAR, according
 %   to the equalization criteria EQTYPE, which can be either ZF (Zero
 %   Forcing) or MMSE (Minimum Mean Square Error). EQSYMBOLS contains the 
 %   equalized OFDM symbols, while EQNOISEVARS contains the equivalent 
 %   post-equalization noise variances, which can be used for soft symbol
-%   demodulation.
+%   demodulation. The TXSCALING scaling factor is the transmit symbol gain
+%   relative to the reference signals used for channel estimation.
 %
 %   RXSYMBOLS is a three-dimensional array. The first dimension corresponds
 %   to the OFDM subcarriers, the second one to the OFDM symbols and the
