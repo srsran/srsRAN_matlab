6621f463bdb6c3a15c2701ef34d97ff198d16ea4
dvdgrgrtt <davidg@srs.io>
2022-09-23 15:39:38 +0200

    Wrapper for nrPUCCH1

---
+++ +srsMatlabWrappers/+phy/+upper/+channel_processors/srsPUCCH1.m
@@ -0,0 +1,20 @@
+%srsPUCCH1 Physical uplink control channel Format 1 modulator.
+%   [SYMBOLS, INDICES] = srsPUCCH1(CARRIER, PUCCH, ACK, SR)
+%   modulates a PUCCH Format 1 message containing the HARQ acknowledgment bits
+%   provided by ACK and the scheduling request provided by SR. It returns the
+%   complex symbols SYMBOLS as well as a column vector of RE indices INDICES.
+%
+%   See also nrPUCCH1 and nrPUCCH1.
+function [symbols, indices] = srsPUCCH1(carrier, pucch, ack, sr)
+
+    FrequencyHopping = 'disabled';
+    if strcmp(pucch.FrequencyHopping, 'intraSlot')
+        FrequencyHopping = 'enabled';
+    end
+
+    symbols = nrPUCCH1(ack, sr, pucch.SymbolAllocation, ...
+        carrier.CyclicPrefix, carrier.NSlot, carrier.NCellID, ...
+        pucch.GroupHopping, pucch.InitialCyclicShift, FrequencyHopping, ...
+        pucch.OCCI);
+    indices = nrPUCCHIndices(carrier, pucch, 'IndexStyle', 'subscript', 'IndexBase', '0based');
+end
