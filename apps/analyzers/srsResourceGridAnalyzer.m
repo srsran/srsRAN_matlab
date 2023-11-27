ba11bddbb322aca2df7ef334959c2d10a5d699c0
dvdgrgrtt <davidg@srs.io>
2023-11-27 16:05:21 +0100

    analyzers: remove unused input parameter

--- apps/analyzers/srsResourceGridAnalyzer.m
+++ apps/analyzers/srsResourceGridAnalyzer.m
@@ -1,11 +1,9 @@
 %srsResourceGridAnalyzer Plots the heat map of a resource grid.
-%   srsResourceGridAnalyzer(NRBS, CYCLICPREFIX, RGFILENAME, RGOFFSET, RGSIZE)
-%   displays the content (amplitude) of a resource grid of NRBS resource blocks
-%   (frequency domain) and one slot (time domain).  The number of symbols in
-%   the slot is determined from the cyclic prefix type CYCLICPREFIX. RGOFFSET
-%   and RGSIZE are the offset and size, as
-%   a number of single-precision complex floating point numbers, of the slot
-%   to be analyzed inside the binary file.
+%   srsResourceGridAnalyzer(NRBS, RGFILENAME, RGOFFSET, RGSIZE) displays the
+%   content (amplitude) of a resource grid of NRBS resource blocks (frequency
+%   domain) and one slot (time domain). RGOFFSET and RGSIZE are the offset
+%   and size, as a number of single-precision complex floating point numbers,
+%   of the slot to be analyzed inside the binary file.
 %
 %   RG = srsResourceGridAnalyzer(...) also returns a matrix RG with the complex-
 %   valued samples of the resource grid (rows are subcarriers, columns are symbols).
@@ -25,10 +23,9 @@
 %   A copy of the BSD 2-Clause License can be found in the LICENSE
 %   file in the top-level directory of this distribution.
 
-function RG = srsResourceGridAnalyzer(nRBs, cyclicPrefix, rgFilename, rgOffset, rgSize)
+function RG = srsResourceGridAnalyzer(nRBs, rgFilename, rgOffset, rgSize)
     arguments
         nRBs (1, 1) double {mustBeInteger, mustBePositive}
-        cyclicPrefix char {mustBeMember(cyclicPrefix, {'normal', 'extended'})}
         rgFilename char {mustBeFile}
         rgOffset (1, 1) double {mustBeInteger, mustBeNonnegative}
         rgSize (1, 1) double {mustBeInteger, mustBePositive}
