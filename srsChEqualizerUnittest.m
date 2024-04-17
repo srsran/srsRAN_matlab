fe2f2b279c4396747959c14628f7f4b18be560e7
Xavier Arteaga <xavier@srs.io>
2024-04-17 18:18:26 +0200

    Refactor channel equalizer test

--- srsChEqualizerUnittest.m
+++ srsChEqualizerUnittest.m
@@ -69,6 +69,9 @@ classdef srsChEqualizerUnittest < srsTest.srsBlockUnittest
     end
 
     properties (TestParameter)
+        % Number of RE to equalize.
+        NumSymbols = {12, 123, 997}
+
         %Channel dimensions.
         %   The first entry is the number of receive antenna ports, the
         %   second entry is the number of transmit layers.
@@ -77,6 +80,9 @@ classdef srsChEqualizerUnittest < srsTest.srsBlockUnittest
         %Equalizer type.
         %   MMSE or ZF.
         eqType = {'MMSE', 'ZF'}
+
+        % Amplitude scaling of the data symbols relative to the reference signals.
+        txScaling = {1, sqrt(2), 0.5}
     end
 
     properties (Hidden)
@@ -109,108 +115,80 @@ classdef srsChEqualizerUnittest < srsTest.srsBlockUnittest
         %   and variable declarations) to the header file pointed by FILEID, which
         %   describes the test vectors.
         fprintf(fileID, [...
-            'template <typename T>\n' ...
-            'struct re_exploded {\n' ...
-            '  unsigned       nof_prb, nof_symbols, nof_slices;\n' ...
-            '  file_vector<T> measurements;\n' ...
+            'struct context_t {\n' ...
+            '  unsigned    nof_re, nof_layers, nof_rx_ports;\n' ...
+            '  float       noise_var;\n' ...
+            '  float       scaling;\n' ...
+            '  std::string equalizer_type;\n' ...
             '};\n'...
-            '\n' ...
-            'struct ch_estimates_exploded {\n' ...
-            '  unsigned          nof_prb, nof_symbols, nof_rx_ports, nof_tx_layers;\n' ...
-            '  float             noise_var;\n' ...
-            '  file_vector<cf_t> estimates;\n' ...
-            '};\n' ...
-            '\n' ...
             'struct test_case_t {\n' ...
-            '  re_exploded<cf_t>     equalized_symbols;\n' ...
-            '  re_exploded<float>    equalized_noise_vars;\n' ...
-            '  re_exploded<cf_t>     transmitted_symbols;\n' ...
-            '  re_exploded<cf_t>     received_symbols;\n' ...
-            '  ch_estimates_exploded ch_estimates;\n' ...
-            '  float                 scaling;\n' ...
-            '  std::string           equalizer_type;\n' ...
+            '  context_t          context;\n' ...
+            '  file_vector<cf_t>  equalized_symbols;\n' ...
+            '  file_vector<float> equalized_noise_vars;\n' ...
+            '  file_vector<cf_t>  received_symbols;\n' ...
+            '  file_vector<cf_t>  ch_estimates;\n' ...
             '};\n'...
             ]);
         end
     end % of methods (Access = protected)
 
     methods (Test, TestTags = {'testvector'})
-        function testvectorGenerationCases(obj, channelSize, eqType)
+        function testvectorGenerationCases(obj, NumSymbols, channelSize, eqType, txScaling)
         %testvectorGenerationCases Generates a test vector for the given
         %   channel size and equalizer type.
             import srsTest.helpers.writeComplexFloatFile
             import srsTest.helpers.writeFloatFile
-            import srsTest.helpers.cellarray2str
+            import srsLib.phy.upper.equalization.srsChannelEqualizer
 
             % Generate a unique test ID by looking at the number of files
             % generated so far.
             testID = obj.generateTestID;
+            
+            % Extract number of receive ports and transmit layers.
+            NumRxPorts = channelSize(1);
+            NumLayers = channelSize(2);
 
-            % Create the channel estimates.
-            obj.createChTensor(channelSize);
+            % Create random receive symbols.
+            rxSymbols = randn(NumSymbols, NumRxPorts) + ...
+                1i * randn(NumSymbols, NumRxPorts);
+
+            % Create random estimated channel.
+            chEsts = randn(NumSymbols, NumRxPorts, NumLayers) + ...
+                1i * randn(NumSymbols, NumRxPorts, NumLayers);
+            
+            % Select a random noise variance between (0.5, 1.5).
+            noiseVar = 0.5 + rand();
 
             % Generate and process the symbols.
-            [eqSymbols, txSymbols, rxSymbols, eqNoiseVars] = obj.runCase(eqType, obj.beta);
+            [eqSymbols, eqNoiseVars] = srsChannelEqualizer(rxSymbols, ...
+                chEsts, eqType, noiseVar, txScaling);
 
-            [~, nSymbols, nRx, nTx] = size(obj.channelTensor);
-            noiseVar = 10^(-obj.snr/10);
-
-            txSymbolDimensions = {...
-                obj.nRB, ...           % nof_prb
-                nSymbols, ...          % nof_symbols
-                nTx, ...               % nof_slices
+            testCaseContext = {...
+                NumSymbols, ... % nof_re
+                NumLayers, ...  % nof_layers
+                NumRxPorts, ... % nof_rx_ports
+                noiseVar, ...   % noise_var
+                txScaling, ...  % scaling
+                eqType, ...     % equalizer_type
                 };
-
-            rxSymbolDimensions = {...
-                obj.nRB, ...           % nof_prb
-                nSymbols, ...          % nof_symbols
-                nRx, ...               % nof_slices
-                };
-
-            chEstimateParams = {...
-                obj.nRB, ...           % nof_prb
-                nSymbols, ...          % nof_symbols
-                nRx, ...               % nof_rx_ports  
-                nTx, ...               % nof_tx_layers
-                noiseVar, ...          % noise_var
-            };
-
-            eqTestParams = {...
-                obj.beta, ...         % scaling
-                ['"' eqType '"'], ... % equalizer_type
-                };
-
 
             % Write the equalized symbols to a binary file.
             obj.saveDataFile('_test_output_eq_symbols', testID, @writeComplexFloatFile, eqSymbols(:));
-            eqString = obj.testCaseToString(testID, txSymbolDimensions, false, '_test_output_eq_symbols');
-            eqString = strrep(eqString, newline, '');
 
             % Write the post-equalization noise variances to a binary file.
             obj.saveDataFile('_test_output_eq_noise_vars', testID, @writeFloatFile, eqNoiseVars(:));
-            eqNoiseString = obj.testCaseToString(testID, txSymbolDimensions, false, '_test_output_eq_noise_vars');
-            eqNoiseString = strrep(eqNoiseString, newline, '');
-            
-            % Write the transmitted symbols to a binary file.
-            obj.saveDataFile('_test_check_tx_symbols', testID, @writeComplexFloatFile, txSymbols(:));
-            txString = obj.testCaseToString(testID, txSymbolDimensions, false, '_test_check_tx_symbols');
-            txString = strrep(txString, newline, '');
 
             % Write the received symbols to a binary file.
             obj.saveDataFile('_test_input_rx_symbols', testID, @writeComplexFloatFile, rxSymbols(:));
-            rxString = obj.testCaseToString(testID, rxSymbolDimensions, false, '_test_input_rx_symbols');
-            rxString = strrep(rxString, newline, '');
 
             % Write the channel estimates to a binary file.
             obj.saveDataFile('_test_input_ch_estimates', testID, @writeComplexFloatFile, obj.channelTensor(:));
-            chEstString = obj.testCaseToString(testID, chEstimateParams, false, '_test_input_ch_estimates');
-            chEstString = strrep(chEstString, sprintf(',\n'), '');
 
-            % Generate the test case string.
-            dataString = {[eqString eqNoiseString txString rxString chEstString]};
-            eqTestParams = [dataString eqTestParams];
-
-            testCaseString = sprintf("%s,\n", cellarray2str(eqTestParams, true));
+            % Generate the test case entry.
+            testCaseString = obj.testCaseToString(testID, ...
+                testCaseContext, true, '_test_output_eq_symbols', ...
+                '_test_output_eq_noise_vars', '_test_input_rx_symbols', ...
+                '_test_input_ch_estimates');
 
             % Add the test to the file header.
             obj.addTestToHeaderFile(obj.headerFileID, testCaseString);
